5a04f0d032d65e9041ba078c5cd044f0260a46fc
Tanmay Vaij <tanmayvaij22@gmail.com>
2024-06-28 22:55:27 +0530

    rough cli version completed
    Signed-off-by: Tanmay Vaij <tanmayvaij22@gmail.com>

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import os
 
+
+def read_file(resource_path: str):
+    with open(resource_path) as f:
+        print(f.read())
+
+
 def scanDir(dir: str):
 
     dirlist = os.listdir(dir)
     for resource in dirlist:
 
-        if os.path.isfile(resource) and resource.endswith(".tsx") : 
-            print(resource)
+        resource_path = f"{dir}/{resource}"
 
-        elif os.path.isdir(resource):
-            scanDir(dir=dir+f"/{resource}")
+        if os.path.isfile(resource_path) and resource.endswith(".tsx"):
+            read_file(resource_path)
 
-        
+        elif os.path.isdir(resource_path):
+            scanDir(dir=dir + f"/{resource}")
+
 
 scanDir(".")
